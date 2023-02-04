55fa44afa7162da7f628b7e6f73519e1d4d2c649
Jim McKeown <mckeown.james@gmail.com>
2023-02-04 09:19:26 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """Returns the todos from a given list"""
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in the text file."""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
